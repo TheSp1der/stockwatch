7dced9fb4b7909e79f96fb36963c0af11c2067a4
TheSp1der <TheSp1der@github.com>
2018-09-17 06:54:51 -0500

    initial commit

---
+++ entrypoint.go
@@ -0,0 +1,218 @@
+/******************************************************************************
+	entrypoint.go
+	This is the entrypoint for the go process. All other functions are
+	initiated from this main function.
+
+	Global variables are also defined here as a matter of convenience.
+******************************************************************************/
+
+package main
+
+// import external libaries
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+
+	"github.com/TheSp1der/goerror"
+	"github.com/fatih/color"
+)
+
+// global variables
+var (
+	cmdLnStocks       string
+	cmdLnEmailAddress string
+	cmdLnEmailHost    string
+	cmdLnEmailPort    int
+	cmdLnEmailFrom    string
+
+	trackedTickers []string
+
+	timeFormat = "2006-01-02 15:04:05"
+)
+
+// init ...
+// ----
+// process initialization
+//
+// input:
+//
+// return:
+func init() {
+	var (
+		err     error
+		tickers string
+	)
+
+	// read command line options
+	flag.StringVar(&cmdLnStocks, "ticker", "", "Comma seperated list of stocks to report (TICKER)")
+	flag.StringVar(&cmdLnEmailAddress, "email", "", "To address or EOD message (EMAIL_ADDR)")
+	flag.StringVar(&cmdLnEmailHost, "host", "localhost", "E-Mail server hostname (EMAIL_HOST)")
+	flag.IntVar(&cmdLnEmailPort, "port", 25, "E-Mail server port (EMAIL_PORT)")
+	flag.StringVar(&cmdLnEmailFrom, "from", "StockWatch <noreply@localhost>", "Address to send mail from (EMAIL_FROM)")
+	flag.Parse()
+
+	// read options from environment variables
+	if cmdLnStocks == "" && len(os.Getenv("TICKER")) > 0 {
+		tickers = strings.ToLower(os.Getenv("TICKER"))
+	}
+	if cmdLnEmailAddress == "" && len(os.Getenv("EMAIL_ADDR")) > 0 {
+		cmdLnEmailAddress = os.Getenv("EMAIL_ADDR")
+	}
+	if cmdLnEmailHost == "" && len(os.Getenv("EMAIL_HOST")) > 0 {
+		cmdLnEmailHost = os.Getenv("EMAIL_HOST")
+	}
+	if cmdLnEmailPort == 25 && len(os.Getenv("EMAIL_PORT")) > 0 {
+		if cmdLnEmailPort, err = strconv.Atoi(os.Getenv("EMAIL_PORT")); err != nil {
+			goerror.Fatal(errors.New("EMAIL_PORT must be numeric."))
+		}
+	}
+	if cmdLnEmailFrom == "StockWatch <noreply@localhost>" && len(os.Getenv("EMAIL_FROM")) > 0 {
+		cmdLnEmailFrom = os.Getenv("EMAIL_FROM")
+	}
+
+	// get tickers from command line (this overrides environment variables)
+	if len(cmdLnStocks) > 0 {
+		tickers = strings.ToLower(cmdLnStocks)
+	}
+
+	// convert input to struct
+	re := regexp.MustCompile(`(\s+)?,(\s+)?`)
+	trackedTickers = re.Split(tickers, -1)
+
+	// verify stocks were provided
+	if len(trackedTickers) == 1 && trackedTickers[0] == "" {
+		goerror.Fatal(errors.New("no Stocks defined"))
+	} else {
+		re = regexp.MustCompile(`^[a-z0-9]+$`)
+		for _, value := range trackedTickers {
+			if !re.Match([]byte(value)) {
+				goerror.Fatal(errors.New("stock ticker format error"))
+			}
+		}
+	}
+}
+
+// main ...
+// ----
+// process entrypoint
+//
+// input:
+//
+// return:
+func main() {
+	var (
+		err     error
+		resp    []byte
+		headers httpHeader
+
+		newURL url.URL
+		params url.Values
+
+		stockData iex
+	)
+
+	// prepare the url
+	newURL.Scheme = "https"
+	newURL.Host = "api.iextrading.com"
+	newURL.Path = "1.0/stock/market/batch"
+
+	// url parameters
+	params = newURL.Query()
+	params.Add("symbols", strings.Join(trackedTickers, ","))
+	params.Add("types", "quote,price,company,stats,ohlc")
+	newURL.RawQuery = params.Encode()
+
+	// connect and retrieve data from remote source
+	if resp, err = httpGet(newURL.String(), headers); err != nil {
+		goerror.Warning(err)
+	}
+
+	// unmarshal response
+	if err = json.Unmarshal(resp, &stockData); err != nil {
+		goerror.Warning(err)
+	}
+
+	// sort
+	keys := make([]string, 0, len(stockData))
+	for k := range stockData {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	fmt.Println("Stock report as of " + color.BlueString(time.Now().Format(timeFormat)))
+	fmt.Println("Company" + "\t\t" + "Current Price" + "\t" + "Change")
+	for _, k := range keys {
+		if stockData[k].Quote.Change < 0 {
+			fmt.Println(stockData[k].Company.Symbol + "\t\t" + strconv.FormatFloat(stockData[k].Price, 'f', 2, 64) + "\t\t" + color.RedString(strconv.FormatFloat(stockData[k].Quote.Change, 'f', 2, 64)) + "\t\t" + stockData[k].Company.CompanyName)
+		} else if stockData[k].Quote.Change > 0 {
+			fmt.Println(stockData[k].Company.Symbol + "\t\t" + strconv.FormatFloat(stockData[k].Price, 'f', 2, 64) + "\t\t" + color.GreenString(strconv.FormatFloat(stockData[k].Quote.Change, 'f', 2, 64)) + "\t\t" + stockData[k].Company.CompanyName)
+		} else {
+			fmt.Println(stockData[k].Company.Symbol + "\t\t" + strconv.FormatFloat(stockData[k].Price, 'f', 2, 64) + "\t\t\t\t" + stockData[k].Company.CompanyName)
+		}
+	}
+
+	var shares float64 = 865
+	investment := (shares * 44.51)
+	value := (shares * stockData["BP"].Price)
+
+	fmt.Println("\n" + strconv.FormatFloat(value-investment, 'f', 2, 64))
+}
+
+func httpGet(url string, header httpHeader) ([]byte, error) {
+	var (
+		err    error
+		client http.Client
+		req    *http.Request
+		resp   *http.Response
+		output []byte
+	)
+
+	// set timeouts
+	client = http.Client{
+		Timeout: time.Duration(2 * time.Second),
+		Transport: &http.Transport{
+			Dial: (&net.Dialer{
+				Timeout: time.Duration(2 * time.Second),
+			}).Dial,
+			TLSHandshakeTimeout: time.Duration(2 * time.Second),
+		},
+	}
+
+	// set up request
+	if req, err = http.NewRequest("GET", url, nil); err != nil {
+		return []byte(""), err
+	}
+
+	// set headers
+	if len(header) > 0 {
+		for _, h := range header {
+			req.Header.Set(h.Name, h.Value)
+		}
+	}
+
+	// perform the request
+	if resp, err = client.Do(req); err != nil {
+		return []byte(""), err
+	}
+
+	// convert the response to byte
+	output, err = ioutil.ReadAll(resp.Body)
+
+	// close the connection
+	defer resp.Body.Close()
+
+	return output, nil
+}
---
+++ types.go
@@ -0,0 +1,137 @@
+package main
+
+type httpHeader []struct {
+	Name  string
+	Value string
+}
+
+type iex map[string]iexData
+
+type iexData struct {
+	Quote   iexQuote   `json:"quote"`
+	Price   float64    `json:"price"`
+	Company iexCompany `json:"company"`
+	Stats   iexStats   `json:"stats"`
+	Ohlc    iexOhlc    `json:"ohlc"`
+}
+
+type iexQuote struct {
+	AvgTotalVolume        int64   `json:"avgTotalVolume"`
+	CalculationPrice      string  `json:"calculationPrice"`
+	Change                float64 `json:"change"`
+	ChangePercent         float64 `json:"changePercent"`
+	Close                 float64 `json:"close"`
+	CloseTime             int64   `json:"closeTime"`
+	CompanyName           string  `json:"companyName"`
+	DelayedPrice          float64 `json:"delayedPrice"`
+	DelayedPriceTime      int64   `json:"delayedPriceTime"`
+	ExtendedChange        float64 `json:"extendedChange"`
+	ExtendedChangePercent float64 `json:"extendedChangePercent"`
+	ExtendedPrice         float64 `json:"extendedPrice"`
+	ExtendedPriceTime     int64   `json:"extendedPriceTime"`
+	High                  float64 `json:"high"`
+	IexAskPrice           float64 `json:"iexAskPrice"`
+	IexAskSize            int64   `json:"iexAskSize"`
+	IexBidPrice           float64 `json:"iexBidPrice"`
+	IexBidSize            int64   `json:"iexBidSize"`
+	IexLastUpdated        int64   `json:"iexLastUpdated"`
+	IexMarketPercent      float64 `json:"iexMarketPercent"`
+	IexRealtimePrice      float64 `json:"iexRealtimePrice"`
+	IexRealtimeSize       int64   `json:"iexRealtimeSize"`
+	IexVolume             int64   `json:"iexVolume"`
+	LatestPrice           float64 `json:"latestPrice"`
+	LatestSource          string  `json:"latestSource"`
+	LatestTime            string  `json:"latestTime"`
+	LatestUpdate          int64   `json:"latestUpdate"`
+	LatestVolume          int64   `json:"latestVolume"`
+	Low                   float64 `json:"low"`
+	MarketCap             int64   `json:"marketCap"`
+	Open                  float64 `json:"open"`
+	OpenTime              int64   `json:"openTime"`
+	PeRatio               float64 `json:"peRatio"`
+	PreviousClose         float64 `json:"previousClose"`
+	PrimaryExchange       string  `json:"primaryExchange"`
+	Sector                string  `json:"sector"`
+	Symbol                string  `json:"symbol"`
+	Week52High            float64 `json:"week52High"`
+	Week52Low             float64 `json:"week52Low"`
+	YtdChange             float64 `json:"ytdChange"`
+}
+
+type iexCompany struct {
+	Ceo         string   `json:"CEO"`
+	CompanyName string   `json:"companyName"`
+	Description string   `json:"description"`
+	Exchange    string   `json:"exchange"`
+	Industry    string   `json:"industry"`
+	IssueType   string   `json:"issueType"`
+	Sector      string   `json:"sector"`
+	Symbol      string   `json:"symbol"`
+	Tags        []string `json:"tags"`
+	Website     string   `json:"website"`
+}
+
+type iexStats struct {
+	Ebitda              int64       `json:"EBITDA"`
+	EPSSurpriseDollar   interface{} `json:"EPSSurpriseDollar"`
+	EPSSurprisePercent  float64     `json:"EPSSurprisePercent"`
+	Beta                float64     `json:"beta"`
+	Cash                int64       `json:"cash"`
+	CompanyName         string      `json:"companyName"`
+	ConsensusEPS        float64     `json:"consensusEPS"`
+	Day200MovingAvg     float64     `json:"day200MovingAvg"`
+	Day30ChangePercent  float64     `json:"day30ChangePercent"`
+	Day50MovingAvg      float64     `json:"day50MovingAvg"`
+	Day5ChangePercent   float64     `json:"day5ChangePercent"`
+	Debt                int64       `json:"debt"`
+	DividendRate        float64     `json:"dividendRate"`
+	DividendYield       float64     `json:"dividendYield"`
+	Float               int64       `json:"float"`
+	GrossProfit         int64       `json:"grossProfit"`
+	InsiderPercent      interface{} `json:"insiderPercent"`
+	InstitutionPercent  float64     `json:"institutionPercent"`
+	LatestEPS           float64     `json:"latestEPS"`
+	LatestEPSDate       string      `json:"latestEPSDate"`
+	Marketcap           int64       `json:"marketcap"`
+	Month1ChangePercent float64     `json:"month1ChangePercent"`
+	Month3ChangePercent float64     `json:"month3ChangePercent"`
+	Month6ChangePercent float64     `json:"month6ChangePercent"`
+	NumberOfEstimates   int64       `json:"numberOfEstimates"`
+	PeRatioHigh         float64     `json:"peRatioHigh"`
+	PeRatioLow          float64     `json:"peRatioLow"`
+	PriceToBook         float64     `json:"priceToBook"`
+	PriceToSales        float64     `json:"priceToSales"`
+	ProfitMargin        float64     `json:"profitMargin"`
+	ReturnOnAssets      float64     `json:"returnOnAssets"`
+	ReturnOnCapital     interface{} `json:"returnOnCapital"`
+	ReturnOnEquity      float64     `json:"returnOnEquity"`
+	Revenue             int64       `json:"revenue"`
+	RevenuePerEmployee  int64       `json:"revenuePerEmployee"`
+	RevenuePerShare     int64       `json:"revenuePerShare"`
+	SharesOutstanding   int64       `json:"sharesOutstanding"`
+	ShortDate           string      `json:"shortDate"`
+	ShortInterest       int64       `json:"shortInterest"`
+	ShortRatio          float64     `json:"shortRatio"`
+	Symbol              string      `json:"symbol"`
+	TtmEPS              float64     `json:"ttmEPS"`
+	Week52change        float64     `json:"week52change"`
+	Week52high          float64     `json:"week52high"`
+	Week52low           float64     `json:"week52low"`
+	Year1ChangePercent  float64     `json:"year1ChangePercent"`
+	Year2ChangePercent  float64     `json:"year2ChangePercent"`
+	Year5ChangePercent  float64     `json:"year5ChangePercent"`
+	YtdChangePercent    float64     `json:"ytdChangePercent"`
+}
+
+type iexOhlc struct {
+	Close struct {
+		Price float64 `json:"price"`
+		Time  int64   `json:"time"`
+	} `json:"close"`
+	High float64 `json:"high"`
+	Low  float64 `json:"low"`
+	Open struct {
+		Price float64 `json:"price"`
+		Time  int64   `json:"time"`
+	} `json:"open"`
+}
