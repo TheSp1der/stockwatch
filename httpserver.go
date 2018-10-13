819c8cbde02ede39a49f03bc5cebf1d3981f76ec
TheSp1der <nhyatt@smoothnet.org>
2018-10-13 08:58:00 -0500

    Adds simple webserver function for outpur via http

---
+++ httpserver.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"strconv"
+	"time"
+
+	"net/http"
+
+	"github.com/TheSp1der/goerror"
+)
+
+func webRoot(resp http.ResponseWriter, req *http.Request) {
+	resp.Header().Add("Content-Type", "text/html")
+	if req.Method == "GET" {
+		resp.Write([]byte(""))
+	}
+}
+
+func webListener(port int) {
+	ws := http.NewServeMux()
+
+	srv := &http.Server{
+		Addr:    ":" + strconv.Itoa(port),
+		Handler: ws,
+		// time to read request headers
+		ReadTimeout: time.Duration(15 * time.Second),
+		// time from accept to end of response
+		WriteTimeout: time.Duration(10 * time.Second),
+		// time a Keep-Alive connection will be kept idle
+		IdleTimeout: time.Duration(120 * time.Second),
+	}
+
+	ws.HandleFunc("/", webRoot)
+
+	if err := srv.ListenAndServe(); err != nil {
+		goerror.Fatal(err)
+	}
+}
