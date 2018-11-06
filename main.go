22bca4c6c12f1233a902b5bfabbb15fa618bd09f
TheSp1der <nhyatt@smoothnet.org>
2018-11-05 18:18:31 -0600

    Reduces function complexity

--- main.go
+++ main.go
@@ -16,15 +16,7 @@ func main() {
 	go updateStockData(sData)
 
 	if !cmdLnNoConsole {
-		go func() {
-			for {
-				if terminal.IsTerminal(int(os.Stdout.Fd())) {
-					fmt.Printf("\033[0;0H")
-				}
-				fmt.Print(displayTerminal(<-sData))
-				time.Sleep(time.Duration(time.Second * 5))
-			}
-		}()
+		go outputConsole(sData)
 	}
 
 	if cmdLnHTTPPort > 0 && cmdLnHTTPPort < 65535 {
@@ -32,24 +24,36 @@ func main() {
 	}
 
 	if cmdLnEmailAddress != "" && cmdLnEmailFrom != "" && cmdLnEmailHost != "" {
-		go func() {
-			for {
-				open, sleepTime := marketStatus()
-				if !open {
-					time.Sleep(time.Duration(time.Minute * 5))
-					if err := basicMailSend(cmdLnEmailHost+":"+strconv.Itoa(cmdLnEmailPort), cmdLnEmailAddress, cmdLnEmailFrom, "Stock Alert", displayHTML(<-sData)); err != nil {
-						goerror.Warning(err)
-					}
-				}
-
-				time.Sleep(time.Duration(sleepTime))
-			}
-		}()
+		go notifyViaMail(sData)
 	}
 
 	if !cmdLnNoConsole || (cmdLnHTTPPort > 0 && cmdLnHTTPPort < 65535) || (cmdLnEmailAddress != "" && cmdLnEmailFrom != "" && cmdLnEmailHost != "") {
 		for {
 			time.Sleep(time.Duration(time.Second * 5))
 		}
+	}
+}
+
+func notifyViaMail(sData chan iex) {
+	for {
+		open, sleepTime := marketStatus()
+		if !open {
+			time.Sleep(time.Duration(time.Minute * 5))
+			if err := basicMailSend(cmdLnEmailHost+":"+strconv.Itoa(cmdLnEmailPort), cmdLnEmailAddress, cmdLnEmailFrom, "Stock Alert", displayHTML(<-sData)); err != nil {
+				goerror.Warning(err)
+			}
+		}
+
+		time.Sleep(time.Duration(sleepTime))
+	}
+}
+
+func outputConsole(sData chan iex) {
+	for {
+		if terminal.IsTerminal(int(os.Stdout.Fd())) {
+			fmt.Printf("\033[0;0H")
+		}
+		fmt.Print(displayTerminal(<-sData))
+		time.Sleep(time.Duration(time.Second * 5))
 	}
 }
